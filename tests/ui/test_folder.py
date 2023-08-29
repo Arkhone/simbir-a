20f4d11829f5363f9170c08433da99b36c62e77b
Arkhone <95479120+Arkhone@users.noreply.github.com>
2023-08-29 13:37:14 +0300

    Update test_folder.py

--- tests/ui/test_folder.py
+++ tests/ui/test_folder.py
@@ -2,9 +2,9 @@ import settings as s
 from pages.disk_page import DiskPage
 
 
-def test_folder(login):
+def test_folder(login, word_example):
     user = DiskPage(login.driver)
     user.create_folder()
     user.open_folder()
-    user.add_file()
+    user.add_file(word_example)
     assert user.get_filename() == s.FILE_NAME
